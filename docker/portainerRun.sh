e2528469559a90d7184ce22e8fc6da70451bb5f6
bouncychair <chris.brown.nice@gmail.com>
2023-01-13 14:59:34 +0100

    added description for scripts to run portainer

--- docker/portainerRun.sh
+++ docker/portainerRun.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
 echo "Open a webbrowser and got to https://localhost:9443"
+
+# FOR LINUX This script is responsible for easier start of the portainer (docker management tool) for the team mates.
+
+# portainerRunning.bat is made specifically for Windows users. 
